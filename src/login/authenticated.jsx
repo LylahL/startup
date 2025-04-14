a5c6e9fae1f36b8ab790dd237a1045630cf714da
LylahL <leimin304@gmail.com>
2025-04-14 15:47:26 -0600

    authenticated call logout api

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,11 +1,20 @@
-
-import React from 'react';
+import React from "react";
 
 export default function Authenticated({ username, onLogout }) {
+  const handleLogout = () => {
+    fetch("/api/auth/logout", { method: "DELETE" })
+      .then(() => {
+        localStorage.removeItem("username");
+        onLogout();
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
     <div className="login-container">
       <h2>Welcome, {username}!</h2>
-      <button onClick={onLogout} className="btn-main color1 color1b">
+      <button onClick={handleLogout} className="btn-main color1 color1b">
         Sign Out
       </button>
     </div>

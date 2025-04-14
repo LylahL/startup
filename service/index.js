5a0fb282c2799bbf66bf80d9f94b5ad2362bc55c
LylahL <leimin304@gmail.com>
2025-04-14 15:18:20 -0600

    create user endpoint

--- service/index.js
+++ service/index.js
@@ -1,2 +1,27 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+// imemory storage for users and scores
+let users = [];
+let scores = [];
+// prefix
+const apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+//create user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await createUser(req.body.email, req.body.password);
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+    }
+  });

458ff5bd9a4463b763276963dd648513d5d86fc7
LylahL <leimin304@gmail.com>
2025-04-15 23:56:50 -0600

    initialize database.js

---
+++ service/database.js
@@ -0,0 +1,8 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+let db; 
+
+

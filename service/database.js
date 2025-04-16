1858dc33ca364400d1402408df12c74d31a3225b
LylahL <leimin304@gmail.com>
2025-04-15 23:58:14 -0600

    get collections

--- service/database.js
+++ service/database.js
@@ -1,8 +1,22 @@
-const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
+const { MongoClient } = require("mongodb");
+const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-let db; 
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+let db;
 
+//get collections
+function getUsersCollection() {
+  return db.collection("users");
+}
 
+function getSavedDesignsCollection() {
+  return db.collection("savedDesigns");
+}
+
+function getPostedDesignsCollection() {
+  return db.collection("postedDesigns");
+}
